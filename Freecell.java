683449f87b389483bd2caa29a60666f90f87f5a6
manyimy <wong,manyi19@gmail.com>
2020-01-10 17:44:20 +0800

    Second commit

--- Freecell.java
+++ Freecell.java
@@ -2,33 +2,42 @@ import java.util.*;
 
 public class Freecell {
   public static void main(String[] args) {
-    Random rand = new Random();
-    ArrayList<String> cards = new ArrayList<>(52);
-    ArrayList<String> pileC = new ArrayList<>();
-    ArrayList<String> pileD = new ArrayList<>();
-    ArrayList<String> pileH = new ArrayList<>();
-    ArrayList<String> pileS = new ArrayList<>();
-    ArrayList<String> column1 = new ArrayList<>();
-    ArrayList<String> column2 = new ArrayList<>();
-    ArrayList<String> column3 = new ArrayList<>();
-    ArrayList<String> column4 = new ArrayList<>();
-    ArrayList<String> column5 = new ArrayList<>();
-    ArrayList<String> column6 = new ArrayList<>();
-    ArrayList<String> column7 = new ArrayList<>();
-    ArrayList<String> column8 = new ArrayList<>();
-    ArrayList<String> column9 = new ArrayList<>();
+    ArrayList<Card> cards = new ArrayList<>(52);
+    ArrayList<Card> pileC = new ArrayList<>();
+    ArrayList<Card> pileD = new ArrayList<>();
+    ArrayList<Card> pileH = new ArrayList<>();
+    ArrayList<Card> pileS = new ArrayList<>();
+    ArrayList<Card> column1 = new ArrayList<>();
+    ArrayList<Card> column2 = new ArrayList<>();
+    ArrayList<Card> column3 = new ArrayList<>();
+    ArrayList<Card> column4 = new ArrayList<>();
+    ArrayList<Card> column5 = new ArrayList<>();
+    ArrayList<Card> column6 = new ArrayList<>();
+    ArrayList<Card> column7 = new ArrayList<>();
+    ArrayList<Card> column8 = new ArrayList<>();
+    ArrayList<Card> column9 = new ArrayList<>();
 
     // suit: spades(s), hearts (H), clubs (c), diamonds (d)
     String[] suit = {"c","d","h","s"};
     // face: number on the card
     String[] face = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K"};
-    for(int i=0; i<4; ++i){
+    /*for(int i=0; i<4; ++i){
       for(int j=0; j<13; ++j){
         String s = suit[i] + face[j];
         cards.add(s);
       }
+    }*/
+
+    for(int i=0; i<4; ++i){
+      for(int j=0; j<13; ++j){
+        String s = suit[i] + face[j];
+        Card c = new Card(s);
+        cards.add(c);
+      }
     }
+
     Collections.shuffle(cards); //shuffle the order of cards
+
     for(int i=0; i<7; ++i)
       column1.add(cards.get(i));
     for(int i=7; i<14; ++i)
@@ -46,19 +55,32 @@ public class Freecell {
     for(int i=46; i<52; ++i)
       column8.add(cards.get(i));
 
+    System.out.print("Column 1: ");
     System.out.println(column1);
+
+    System.out.print("Column 2: ");
     System.out.println(column2);
+
+    System.out.print("Column 3: ");
     System.out.println(column3);
+
+    System.out.print("Column 4: ");
     System.out.println(column4);
+
+    System.out.print("Column 5: ");
     System.out.println(column5);
+
+    System.out.print("Column 6: ");
     System.out.println(column6);
+
+    System.out.print("Column 7: ");
     System.out.println(column7);
+
+    System.out.print("Column 8: ");
     System.out.println(column8);
+
+    System.out.print("Column 9: ");
     System.out.println(column9);
-
-    Card c1 = new Card(cards.get(0));
-    System.out.println(c1.toString());
-
   }
 
 }
@@ -75,6 +97,10 @@ class Card {
     setFace();
     setPoint();
   }
+  public String getName() { return name; }
+  public char getFace() { return face; }
+  public char getSuit() { return suit; }
+  public int getPoint() { return point; }
   public void setSuit() {
     suit = name.charAt(0);
   }
@@ -91,7 +117,10 @@ class Card {
       default: point = Character.getNumericValue(face);break;
     }
   }
+  // public String toString(){
+  //   return "Name: " + name + ", Suit: " + suit + ", Face: " + face + ", Point: " + point;
+  // }
   public String toString(){
-    return "Name: " + name + ", Suit: " + suit + ", Face: " + face + ", Point: " + point;
+    return name;
   }
 }
